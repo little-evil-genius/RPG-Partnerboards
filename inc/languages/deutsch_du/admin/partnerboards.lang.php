3e8fe8cdc01f6a88f034214a3cf52cb3088b53db
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-10-21 19:34:27 +0200

    Create partnerboards.lang.php

---
+++ inc/languages/deutsch_du/admin/partnerboards.lang.php
@@ -0,0 +1,78 @@
+<?php
+$l['partnerboards_error_rpgstuff'] = "Das ACP Modul <a href=\"https://github.com/little-evil-genius/rpgstuff_modul\" target=\"_blank\">\"RPG Stuff\"</a> muss vorhanden sein!";
+
+$l['partnerboards_permission'] = "Kann die Felder für den Partnerbereich verwalten?";
+$l['partnerboards_nav'] = "Eigene Partnerbereichfelder";
+
+$l['partnerboards_breadcrumb_main'] = "Eigene Partnerbereichfelder";
+$l['partnerboards_breadcrumb_add'] = "Neues Partnerbereichfeld hinzufügen";
+$l['partnerboards_breadcrumb_edit'] = "Partnerbereichfeld bearbeiten";
+
+$l['partnerboards_tabs_overview'] = "Eigene Partnerbereichfelder";
+$l['partnerboards_tabs_overview_desc'] = "In diesem Bereich kannst du eigene Partnerbereichfelder verwalten, erstellen und löschen.";
+$l['partnerboards_tabs_add'] = "Neues Partnerbereichfeld hinzufügen";
+$l['partnerboards_tabs_add_desc'] = "Hier kannst du ein eigenes Partnerbereichfeld erstellen.";
+$l['partnerboards_tabs_edit'] = "Partnerbereichfeld bearbeiten";
+$l['partnerboards_tabs_edit_desc'] = "Hier kannst du ein eigenes Partnerbereichfelder bearbeiten.";
+
+$l['partnerboards_overview_header'] = "Eigene Partnerbereichfelder";
+$l['partnerboards_overview_container'] = "Eigene Partnerbereichfelder";
+$l['partnerboards_overview_container_field'] = "Partnerbereichfelder";
+$l['partnerboards_overview_container_require'] = "Pflichtfeld";
+$l['partnerboards_overview_container_sort'] = "Sortierung";
+$l['partnerboards_overview_container_options'] = "Steuerung";
+$l['partnerboards_overview_yes'] = "Ja";
+$l['partnerboards_overview_no'] = "Nein";
+$l['partnerboards_overview_options_edit'] = "Feld bearbeiten";
+$l['partnerboards_overview_options_delete'] = "Feld löschen";
+$l['partnerboards_overview_options_delete_notice'] = "Soll dieses Partnerbereichfeld wirklich gelöscht werden?";
+$l['partnerboards_overview_none'] = "Bisher wurden keine eigenen Partnerbereichfelder hinzugefügt.";
+$l['partnerboards_overview_sort_button'] = "Partnerbereichfelder-Sortierung speichern";
+$l['partnerboards_overview_sort_flash'] = "Die Anzeigereihenfolge der Partnerbereichfelder wurde erfolgreich aktualisiert.";
+
+$l['partnerboards_container_identification'] = "Identifikator <em>*</em>";
+$l['partnerboards_container_identification_desc'] = "Dieser Identifikator dient als Orientierung in der Datenbank und für die Variabel-Bezeichnung im Template. Es sind keine Sonderzeichen oder Leerzeichen erlaubt (maschinenlesbar). Der Name muss für jedes Feld unterschiedlich sein.";
+$l['partnerboards_container_title'] = "Titel <em>*</em>";
+$l['partnerboards_container_description'] = "Kurzbeschreibung<em>*</em>";
+$l['partnerboards_container_type'] = "Feldtyp <em>*</em>";
+$l['partnerboards_container_type_desc'] = "Dies ist der Feldtyp, der gezeigt wird.";
+$l['partnerboards_container_selectoptions'] = "Auswählbare Optionen?<em>*</em>";
+$l['partnerboards_container_selectoptions_desc'] = "Gib jede Option in eine neue Zeile ein. Diese Einstellung trifft nur bei Auswahlboxen, Checkboxen und Radiobuttons zu.";
+$l['partnerboards_container_disporder'] = "Sortierung <em>*</em>";
+$l['partnerboards_container_disporder_desc'] = "Dies ist die Anzeigereihenfolge, abhängig von anderen Partnerbereichfeldern. Die Zahl sollte für jedes Feld unterschiedlich sein.";
+$l['partnerboards_container_require'] = "Benötigt? <em>*</em>";
+$l['partnerboards_container_require_desc'] = "Muss dieses Feld bei einer Neueröffnung und bei der Bearbeitung von einer Szene ausgefüllt werden?";
+$l['partnerboards_container_editableby'] = "Sichtbar für <em>*</em>";
+$l['partnerboards_container_editableby_desc'] = "Wähle die Gruppen aus, die dieses Partnerbereichfeld sehen und somit ausfüllen können.";
+$l['partnerboards_container_parseroptions'] = "MyCode-Optionen";
+
+$l['partnerboards_type_text'] = "Textbox";
+$l['partnerboards_type_textarea'] = "Textarea";
+$l['partnerboards_type_select'] = "Auswahlbox";
+$l['partnerboards_type_multiselect'] = "Auswahlbox mit mehreren Optionen";
+$l['partnerboards_type_radio'] = "Radiobuttons";
+$l['partnerboards_type_checkbox'] = "Checkboxen";
+$l['partnerboards_type_date'] = "Datum";
+$l['partnerboards_type_url'] = "URL";
+
+$l['partnerboards_container_parse_allowhtml'] = "HTML erlauben";
+$l['partnerboards_container_parse_allowmycode'] = "MyCode erlauben";
+
+$l['partnerboards_error_identification'] = "Du hast keinen Identifikator für dieses Partnerbereichfeld angegeben.";
+$l['partnerboards_error_title'] = "Du hast keinen Titel für dieses Partnerbereichfeld angegeben.";
+$l['partnerboards_error_description'] = "Du hast keine Beschreibung für dieses Partnerbereichfeld angegeben.";
+$l['partnerboards_error_selectoptions'] = "Du hast keine auswählbare Optionen für dieses Partnerbereichfeld angegeben.";
+$l['partnerboards_error_invalid'] = "Ungültige Option";
+$l['partnerboards_error_sort'] = "Beim Feststellen der Partnerbereichfelder-Sortierung ist ein Fehler aufgetreten. Bitte aktualisiere die Seite und probiere es erneut.";
+
+$l['partnerboards_add_header'] = "Neues Partnerbereichfeld hinzufügen";
+$l['partnerboards_add_container'] = "Neues Partnerbereich hinzufügen";
+$l['partnerboards_add_button'] = "Partnerbereich speichern";
+$l['partnerboards_add_flash'] = "Das Partnerbereich wurde erfolgreich erstellt.";
+
+$l['partnerboards_edit_header'] = "Partnerbereichfeld bearbeiten";
+$l['partnerboards_edit_container'] = "Partnerbereichfeld: {1}";
+$l['partnerboards_edit_button'] = "Partnerbereichfeld speichern";
+$l['partnerboards_edit_flash'] = "Das Partnerbereichfeld wurde erfolgreich gespeichert.";
+
+$l['partnerboards_delete_flash'] = "Das Partnerbereichfeld wurde erfolgreich gelöscht.";
