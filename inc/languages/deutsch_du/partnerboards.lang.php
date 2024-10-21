58626cfad5d1db14ed4c4536d2c67711e2c010d3
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-10-21 19:33:49 +0200

    Create partnerboards.lang.php

---
+++ inc/languages/deutsch_du/partnerboards.lang.php
@@ -0,0 +1,26 @@
+<?php
+$l['partnerboards_index'] = "Unsere Partner";
+$l['partnerboards_index_none'] = "Aktuell haben wir noch keine Partnerboards.";
+
+$l['partnerboards_modcp_nav'] = "Partnerforen";
+$l['partnerboards_modcp'] = "Übersicht der Partnerforen";
+$l['partnerboards_modcp_management'] = "Themen in der Verwaltung";
+$l['partnerboards_modcp_adopted'] = "angenommene Partnerforen";
+$l['partnerboards_modcp_thread'] = "Thema";
+$l['partnerboards_modcp_facts'] = "angegebene Informationen";
+$l['partnerboards_modcp_post'] = "gepostet am";
+$l['partnerboards_modcp_none_management'] = "Aktuell gibt es keine Anfragen, Bestätigungen oder ähnliches an Partnerschaften!";
+$l['partnerboards_modcp_none_adopted'] = "Aktuell gibt es noch keine angenommenen Partnerschaften!";
+
+$l['partnerboards_newthread_rules'] = "Unsere Kriterien";
+$l['partnerboards_newthread_indexdisplay_title'] = "Index-Anzeige";
+$l['partnerboards_newthread_indexdisplay_desc'] = "Soll dieses Forum auf dem Index angezeigt werden?";
+
+$l['partnerboards_validate_field'] = "Das Partnerforenfeld \"{1}\" muss ausgefüllt werden.";
+
+$l['partnerboards_overview'] = "Übersicht der Partnerforen";
+$l['partnerboards_overview_thread'] = "Thema";
+$l['partnerboards_overview_facts'] = "angegebene Informationen";
+$l['partnerboards_overview_post'] = "gepostet am";
+$l['partnerboards_overview_none'] = "Aktuell gibt es noch keine angenommenen Partnerschaften!";
+$l['partnerboards_overview_redirect'] = "Die Übersichtseite der Partnerforen wurde deaktiviert. Du wirst nun auf den Index zurückgeleitet.";
